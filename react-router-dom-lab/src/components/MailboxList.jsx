69ebc50131ad4977e0e4fa5c01adc920d03c23d0
joshuanikolas <nikolassnchez@icloud.com>
2024-11-21 13:59:45 -0600

    quan's code, error with his code being pushed

---
+++ react-router-dom-lab/src/components/MailboxList.jsx
@@ -0,0 +1,20 @@
+import { Link } from 'react-router-dom';
+
+const MailboxList = ({ mailboxes }) => { 
+  return (
+    <>
+      <h1>Mailbox List</h1>
+      <ul>
+        {mailboxes.map((chosenMailbox) => (
+          <li key={chosenMailbox._id} className="mail-box">
+            <Link to={`/mailboxes/${chosenMailbox._id}`}>
+              Mailbox {chosenMailbox._id}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default MailboxList;
