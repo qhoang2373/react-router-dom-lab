23517f55b33f9f1c5dec1550f480d8ef13edf3e5
Quan <qhoang2373@gmail.com>
2024-11-21 00:03:26 -0800

    edit mailboxForm

--- react-router-dom-lab/src/components/MailboxDetails.jsx
+++ react-router-dom-lab/src/components/MailboxDetails.jsx
@@ -1,20 +1,20 @@
-import { useParams } from "react-router-dom"
+import { useParams } from "react-router-dom";
 
-const MailboxDetails = ({ props }) => {
-    const { mailboxId } = useParams()
-    const currentMailbox = props.mailboxes.find((mailbox) => mailbox._id === Number(mailboxId))
-   
-    return (
-        <>
-        <h2>MailBox {currentMailbox._id}</h2>
-        <dl>
-          <dt>Box Size: {currentMailbox.boxSize}</dt>
-          <dt>Box Holder: {currentMailbox.boxholder}</dt>
-          <dt>Box Number: {currentMailbox._id}</dt>
-        </dl>
-      </>
-  )
-}
+const MailboxDetails = ({ mailboxes }) => {
+  const { mailboxId } = useParams();
+  const currentMailbox = mailboxes.find((mailbox) => mailbox._id === Number(mailboxId));
+
+  return (
+    <>
+      <h2>Mailbox {currentMailbox._id}</h2>
+      <dl>
+        <dt>Box Size: {currentMailbox.boxSize}</dt>
+        <dt>Box Holder: {currentMailbox.boxholder}</dt>
+        <dt>Box Number: {currentMailbox._id}</dt>
+      </dl>
+    </>
+  );
+};
 
 export default MailboxDetails;
 
--- react-router-dom-lab/src/components/MailboxList.jsx
+++ react-router-dom-lab/src/components/MailboxList.jsx
@@ -1,17 +1,20 @@
 import { Link } from 'react-router-dom';
 
-const MailboxList = ({ mailboxes }) => {
-    return (
-        <>
-         <h1>Mailbox List</h1>
-         <ul>
-         {props.mailboxes.map((chosenMailbox) =>(
-        <li key={chosenMailbox._id} className="mail-box">
+const MailboxList = ({ mailboxes }) => { 
+  return (
+    <>
+      <h1>Mailbox List</h1>
+      <ul>
+        {mailboxes.map((chosenMailbox) => (
+          <li key={chosenMailbox._id} className="mail-box">
             <Link to={`/mailboxes/${chosenMailbox._id}`}>
-            Mailbox {chosenMailbox._id}</Link>
-        </li>
+              Mailbox {chosenMailbox._id}
+            </Link>
+          </li>
         ))}
-        </ul>
-        </>
-    )
-}
+      </ul>
+    </>
+  );
+};
+
+export default MailboxList;
